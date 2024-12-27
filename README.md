8fe3c52525f07568970978a921162f70c75257a2
Panthomema <71283471+Panthomema@users.noreply.github.com>
2024-12-27 15:31:38 +0100

    Update webpack configuration

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # webpack-template
-Webpack template repository
+
+Webpack development template repository for SPA-like front-end only apps that includes:
+
+- A minimal project structure
+- Webpack v5 configuration
+
+## Details
+
+### Project Structure
+
+Following webpack standards:
+
+project/
+- src/
+  - assets/
+  - styles/
+    - reset.css
+  - index.js
+  - template.html
+- dist/
+  - ...(webpack build output)
+- ...(config files)
+
+### Webpack v5 Configuration
+
+Webpack configuration that includes:
+
+- 3 files (common, dev, & prod), that are managed with webpack-merge package & npm scripts
+- HTMLWebpack plugin to build from the template.html file (needs tuning for MPAs)
+- Configured webpack dev server
+- Various configured loaders to handle assets & styles
+
+
+
--- webpack.common.js
+++ webpack.common.js
@@ -2,17 +2,18 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: { 
+  entry: {
     app: './src/index.js',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/template.html",
+      template: './src/template.html',
+      /* favicon: './src/favicon.svg', */
     }),
   ],
   output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
   module: {
@@ -20,6 +21,22 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.json$/,
+        type: 'json',
+      },
+      {
+        test: /\.svg$/,
+        use: 'raw-loader',
       },
     ],
   },
